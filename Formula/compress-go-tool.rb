8692adf4bb1be02eb0977d7624c32af2a698471a
Sunny jain <sunny@Sunnys-MacBook-Air.local>
2025-12-14 16:11:12 +0530

    Add compress-go-tool Homebrew formula

---
+++ Formula/compress-go-tool.rb
@@ -0,0 +1,15 @@
+class compress-go-tool < Formula
+  desc "Simple file compression CLI written in Go"
+  homepage "https://github.com/sunnyjain123/compressor-tool"
+  url "https://github.com/sunnyjain123/compressor-tool/releases/download/v1.0.0/compress-go-tool-darwin-universal.tar.gz"
+  sha256 "PUT_SHA256_HERE"
+  version "1.0.0"
+
+  def install
+    bin.install "compress-go-tool"
+  end
+
+  test do
+    system "#{bin}/compress-go-tool", "--help"
+  end
+end
