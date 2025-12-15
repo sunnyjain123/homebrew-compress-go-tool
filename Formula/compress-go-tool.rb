997c62ebc453464ef70352d11aeec77cae3de5f5
Sunny jain <jain.sunny301@gmail.com>
2025-12-16 00:21:13 +0530

    upgrade to v1.0.4

--- Formula/compress-go-tool.rb
+++ Formula/compress-go-tool.rb
@@ -1,9 +1,9 @@
 class CompressGoTool < Formula
   desc "Simple file compression CLI written in Go"
   homepage "https://github.com/sunnyjain123/compress-go-tool"
-  url "https://github.com/sunnyjain123/compress-go-tool/releases/download/v1.0.3/compress-go-tool-darwin-universal.tar.gz"
-  sha256 "a74aea8b665597b48d8f009020db35f587d1384907a6849803e7b5a9b4dd3fcc"
-  version "1.0.3"
+  url "https://github.com/sunnyjain123/compress-go-tool/releases/download/v1.0.4/compress-go-tool-darwin-universal.tar.gz"
+  sha256 "7f4aa9250b40c259d68fd7d2fb9ed7aae1e02dbe949c6b969ec943494a1da3fc"
+  version "1.0.4"
 
   def install
     bin.install "compress-go-tool"
